c201c58a574b22e1387f247e4a9f3e77c214bc26
Jarime94 <jriveramedina94@gmail.com>
2023-09-25 13:42:43 +0200

    Se agrega el Proyecto

---
+++ ADIVINA_EL_NUMERO.py
@@ -0,0 +1,51 @@
+from random import randint
+
+intentos = 0
+numero_secreto = randint(1,100)
+
+nombre = input("Dime tu nombre: ")
+
+print(f"Bueno {nombre}, he pensado un número entre 1 y 100 \nTienes ocho intentos para adivinar")
+
+while intentos < 8:
+    estimado = int(input("Cuál es el número?: "))
+    intentos += 1
+
+    if estimado < numero_secreto:
+        print("Mi número es más alto")
+
+    if estimado > numero_secreto:
+        print("Mi número es más bajo")
+
+    if estimado == numero_secreto:
+        print(f"Felicidades {nombre}! Has adivinado en {intentos} intentos")
+        break
+
+if estimado != numero_secreto:
+    print(f"Lo siento se han agotado los intentos. El número secreto era {numero_secreto}")
+
+from random import randint
+estimado = 0
+intentos = 0
+numero_secreto = randint(1,100)
+
+nombre = input("Dime tu nombre: ")
+
+print(f"Bueno {nombre}, he pensado un número entre 1 y 100 \nTienes ocho intentos para adivinar")
+
+while intentos < 8:
+    estimado = int(input("Cuál es el número?: "))
+    intentos += 1
+
+    if estimado not in range(1,101):
+        print("Tu número no se encuentra en el rango que va de 1 a 100")
+    elif estimado < numero_secreto:
+        print("Mi número es más alto")
+    elif estimado > numero_secreto:
+        print("Mi número es más bajo")
+    else:
+        print(f"Felicidades {nombre}! Has adivinado en {intentos} intentos")
+        break
+
+if estimado != numero_secreto:
+    print(f"Lo siento se han agotado los intentos. El número secreto era {numero_secreto}")
